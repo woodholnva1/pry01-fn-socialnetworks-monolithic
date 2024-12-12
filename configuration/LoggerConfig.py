9eeb9b115e09d11dc9da409deb73515654b9281b
woodholnva <freddyanf@gmail.com>
2024-12-12 09:43:00 -0800

    Create dir logs if not exist

--- configuration/LoggerConfig.py
+++ configuration/LoggerConfig.py
@@ -5,8 +5,12 @@ from flask.logging import default_handler
 
 # Function to Config Logs
 def configLogs():
+    log_dir = "./logs"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
     # Configurar el logger de errores
-    error_log_handler = logging.FileHandler(os.path.join("./logs", "error.log"))
+    error_log_handler = logging.FileHandler(os.path.join(log_dir, "error.log"))
     error_log_handler.setLevel(logging.ERROR)
     error_log_formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
