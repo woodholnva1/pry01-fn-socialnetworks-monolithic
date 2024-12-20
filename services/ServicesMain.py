62dff4a80e6da0ed41a967ae644e2504f0519b43
woodholnva <freddyanf@gmail.com>
2024-12-20 12:55:49 -0800

    ajustes templates, views y services

--- services/ServicesMain.py
+++ services/ServicesMain.py
@@ -1,12 +1,10 @@
 import logging
-
 from flask import render_template, current_app
-
 from repository.RepositoryTable import RepositoryTable
 
 logger = logging.getLogger(__name__)
-class ServicesMain:
 
+class ServicesMain:
 
     def __init__(self):
         self.repositoryTable = RepositoryTable()
--- views/MainViews.py
+++ views/MainViews.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, current_app
+from flask import Blueprint
 
 from services.ServicesMain import ServicesMain
 
