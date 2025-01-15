4759c8ebeec491e86099d47bbc6333bcb72d85fc
woodholnva <freddyanf@gmail.com>
2025-01-14 16:57:23 -0800

    Ajustes js and validator to pydantic empty value

--- models/beans/registerSurveyRequest.py
+++ models/beans/registerSurveyRequest.py
@@ -1,13 +1,20 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, PositiveInt, validator, field_validator
+
 
 class redes(BaseModel):
-    id_red:int = None
-    value:int  = None
+    id_red:PositiveInt = None
+    value:PositiveInt  = None
 
 class RegisterSurveyRequset(BaseModel):
-    mail:str
-    years:str
-    sex:int
-    favorite_network:int
+    mail:str = Field(..., min_length=1)
+    years:str = Field(..., min_length=1)
+    sex:PositiveInt
+    favorite_network:PositiveInt
     redes_sociales:List[redes]
+
+    @field_validator("redes_sociales")
+    def validar_lista_no_vacia(cls, v):
+        if not v:
+            raise ValueError("La lista de redes sociales no puede estar vacía")
+        return v
